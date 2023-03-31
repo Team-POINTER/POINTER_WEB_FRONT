2a03094e33fadab7ec0a245986e7b0504a159a8d
pbit <alstjqrhkr2018@gmail.com>
2023-03-31 12:33:21 +0900

    Home UI

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div >
+      <div>
+        <img src="./img/Text_logo.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
