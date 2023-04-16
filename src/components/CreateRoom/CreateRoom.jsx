fbec63e49fce55b4ad193a2f49f20e7787d348d1
pbit <alstjqrhkr2018@gmail.com>
2023-04-16 12:26:20 +0900

    Add CreateRoom button, pbit

---
+++ src/components/CreateRoom/CreateRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './CreateRoom.module.css'
+
+export default function CreateRoom() {
+  return (
+    <button className={styles.room}>
+      룸 만들기
+    </button>
+  );
+}
+
