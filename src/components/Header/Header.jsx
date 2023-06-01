2cbd71ffa9d7455298abd557d4fcff5912c11e4f
pbit <alstjqrhkr2018@gmail.com>
2023-06-01 18:03:54 +0900

    너비에 따른 룸만들기 버튼 동적 변경, seopmin

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.css'
 
 export const Header = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
       <button className={styles.logo}>
         <img src="./img/Text_logo.png" alt="" className={styles.logoImg}/>
       </button>
       <div className={styles.empty}></div>
-      <section className={styles.section}>
-        <button className={styles.room}>룸 만들기</button>
+      <nav className={styles.nav}>
+        {windowWidth <= 820 ? (
+          // <button className={styles.addButton}><img className={styles.plusImg} src="./img/x.png" alt="+버튼" /></button>
+          <button className={styles.addButton}>+</button>
+        ) : (
+          <button className={styles.room}>룸 만들기</button>
+        )}
         <button className={styles.user}>포인터 님</button>
-      </section>
+      </nav>
     </header>
   );
 }
