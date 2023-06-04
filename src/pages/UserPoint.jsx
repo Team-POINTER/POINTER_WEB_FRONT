b01c62f901bbacbc6c394a5f89fdb9b1690bdc94
pbit <alstjqrhkr2018@gmail.com>
2023-06-04 12:09:09 +0900

    #2 지목 버튼 create, seopmin

--- src/pages/UserPoint.jsx
+++ src/pages/UserPoint.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Header } from '../components/Header/Header';
+import styled from "styled-components";
 import { HintSection } from '../components/Hint/HintSection';
+
+const PointBtn = styled.button`
+  width: 124px;
+  height: 39px;
+  background-color: #FF2301;
+  border-radius: 1rem;
+  color: white;
+`;
 
 export const UserPoint = () => {
   return (
     <>
       <Header />
       <HintSection />
+      <PointBtn> <img src="/img/POINTER_btn.png" alt="" /> </PointBtn>
     </>
   );
 }
