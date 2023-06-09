27c2faa82f8f6435ab95d752755cd74076bbe98b
seopmin <alstjqrhkr2018@gmail.com>
2023-06-09 16:17:17 +0900

    체크할 시 포인트 효과 생성, seopmin

--- src/components/UserList/UserBox.jsx
+++ src/components/UserList/UserBox.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './UserBox.module.css';
+import Icon from "../../icon/Icon";
 
-export const UserBox = ({ userData }) => {
+export const UserBox = ({ userData, isCheck }) => {
   return (
     <li className={styles.userBox}>
       <div className={styles.textContainer}>
         {userData.userName}
+        <div className={styles.empty}></div>
+        {isCheck && <Icon icon="writerMark" color="red" />}
       </div>
     </li>
   );
