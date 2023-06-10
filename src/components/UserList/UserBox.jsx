273fa5342a32d601cc777cb15709dd854f67d419
pbit <alstjqrhkr2018@gmail.com>
2023-06-10 14:33:02 +0900

    User 선택 후 포인트 버튼 누를 시, 선택된 유저 정보

--- src/components/UserList/UserBox.jsx
+++ src/components/UserList/UserBox.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserBox.module.css';
 import Icon from "../../icon/Icon";
 
-export const UserBox = ({ userData, isCheck }) => {
+export const UserBox = ({ userData, handleUserSelect }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    handleUserSelect(userData); // 사용자 정보를 UserPoint 컴포넌트로 전송
+  };
+
   return (
     <li className={styles.userBox}>
-      <div className={styles.textContainer}>
+      <div className={styles.textContainer} onClick={handleClick}>
         {userData.userName}
         <div className={styles.empty}></div>
-        {isCheck && <Icon icon="writerMark" color="red" />}
+        {isClicked && <Icon icon="writerMark" color="red" />}
       </div>
     </li>
   );
-}
+};
