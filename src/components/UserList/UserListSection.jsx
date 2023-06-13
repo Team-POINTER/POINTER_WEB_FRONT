3633bb41a530d4facf6edf767485741ec3daa5a6
pbit <alstjqrhkr2018@gmail.com>
2023-06-13 18:26:22 +0900

    UserPoint UI 유저 선택 시 리스트에 유저 이름 생성, seopmin

--- src/components/UserList/UserListSection.jsx
+++ src/components/UserList/UserListSection.jsx
@@ -3,7 +3,10 @@ import styled from "styled-components";
 import {v4 as uuidv4} from 'uuid';
 
 const SectionContainer = styled.div`
-  margin-top: 16px;
+  padding: 16px;
+  height: 150px;
+  width: 288px;
+  overflow: auto;
 `;
 
 
@@ -13,21 +16,25 @@ const UserList = styled.ul`
   list-style-type: none;
   padding: 0;
   margin: 8px 0;
+  flex-wrap: wrap;
 `;
 
 const UserName = styled.li`
   font-size: 14px;
   color: white;
+  flex-shrink: 0;
 `;
 
 export const UserListSection = ({ names }) => {
   return (
     <SectionContainer>
       <UserList>
-        {names.map((name) => (
-          <UserName key={uuidv4()}>{name}</UserName>
+        {names.map((name, index) => (
+          
+          <UserName key={uuidv4()}> <span>{index !== 0 && <span> &nbsp;· </span>}{name}</span></UserName>
         ))}
       </UserList>
     </SectionContainer>
   );
 };
+
