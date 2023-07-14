e23b5018ed5ef5747c2ab304385a26cf9481b90c
seopmin <alstjqrhkr2018@gmail.com>
2023-07-14 11:44:06 +0900

    질문 등록, 링크 초대 버튼 컴포넌트 생성

--- src/pages/PointResult.jsx
+++ src/pages/PointResult.jsx
@@ -56,7 +56,69 @@ const Question = styled.p`
   font-style: normal;
   font-weight: 500;
   line-height: 150%;
+  padding: 93px;
 `
+
+const RegisterBtn = styled.div`
+  display: inline-flex;
+  padding: 7px 17px 9px 17px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  border-radius: 999px;
+  background: var(--orangered, #ff2301);
+  color: #fff;
+  text-align: center;
+
+  /* B 16 */
+  font-size: 16px;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%;
+
+  span {
+    /* R 16 */
+    font-weight: 400;
+    line-height: 170%;
+  }
+`;
+
+const LinkCopy = styled.div`
+  display: inline-flex;
+  height: 31px;
+  padding: 6px 18px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  flex-shrink: 0;
+  border-radius: 999px;
+  border: 1px solid var(--white, #fff);
+
+  color: var(--white, #fff);
+  text-align: center;
+  /* B 16 */
+  font-size: 16px;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%;
+`;
+
+const Buttons = styled.div`
+  width: 100%;
+  height: 43px;
+  margin-bottom: 28px;
+  display: flex;
+  justify-content: center;
+  gap: 18px;
+  // @media (min-width: 835px) and (max-width: 1200px) {
+  //   margin-bottom: 54px;
+  // }
+  // @media screen and (max-width: 834px) {
+  //   margin-bottom: 28px;
+  // }
+`;
 
 export const PointResult = ({ room }) => {
   const [userList, setUserList] = useState([]);
@@ -80,6 +142,10 @@ export const PointResult = ({ room }) => {
     <Wrap>
       <Header />
       <Question>{question}</Question>
+      <Buttons>
+        <RegisterBtn>질문 등록하기<span>22:22:11</span></RegisterBtn>
+        <LinkCopy>링크로 초대</LinkCopy>
+      </Buttons>
       <UserResult>
         {userList.map((user, index) => (
           <div key={user.userId}>
