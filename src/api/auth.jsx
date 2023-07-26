f8bc0b69ff40aa9544c66903bd2777b4887a6305
seopmin <alstjqrhkr2018@gmail.com>
2023-07-26 23:06:24 +0900

    ADD: baseurl 옆에 / 추가

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "../function/cookie";
 
 export const getUserInfo = (userId) => {
-  return axios.get(`${process.env.REACT_APP_BASE_URL}users/${userId}/info`, {
+  return axios.get(`${process.env.REACT_APP_BASE_URL}/users/${userId}/info`, {
     headers: {
       Authorization: "Bearer " + getCookie("refreshToken"),
     },
