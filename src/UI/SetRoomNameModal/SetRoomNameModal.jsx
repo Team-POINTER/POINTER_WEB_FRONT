4cfa2bdd4111e7417cf6804d18e75e27ef9aa7e3
seopmin <alstjqrhkr2018@gmail.com>
2023-08-09 22:42:48 +0900

    [FEAT] 룸생성 api 연결

--- src/UI/SetRoomNameModal/SetRoomNameModal.jsx
+++ src/UI/SetRoomNameModal/SetRoomNameModal.jsx
@@ -1,36 +1,52 @@
 import React, { useState } from "react";
 import Card from "../Card/Card";
 import ReactDOM from "react-dom";
-import styles from './SetRoomNameModal.module.css'
+import styles from "./SetRoomNameModal.module.css";
+import { createRoom } from "../../api/room";
+import { useNavigate } from "react-router-dom";
 
 const Backdrop = (props) => {
   return <div className={styles.backdrop} onClick={props.onConfirm} />;
 };
 
 const ModalOverlay = (props) => {
+  const navigate = useNavigate();
   const [text, setText] = useState({
     text: "",
     length: 0,
   });
   const TextHandler = (e) => {
-    if(e.target.value.length > 15) return;
-    setText(prev => ({text: e.target.value, length: e.target.value.length}));
-  }
+    if (e.target.value.length > 15) return;
+    setText((prev) => ({
+      text: e.target.value,
+      length: e.target.value.length,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createRoom(text.text);
+    window.location.replace("/home");
+  };
   return (
     <Card className={styles.modal}>
-      <header className={styles.header}>
-        <p>새로운 룸의 이름 설정</p>
-      </header>
-      <div className={styles.content}>
-        <input value={text.text} onChange={TextHandler} type="text" />
-        <span>{text.length}/15</span>
-      </div>
-      <hr />
-      <footer className={styles.actions}>
-        <button onClick={props.onConfirm}>취소</button>
-        <div className={styles.updown} />
-        <button>완료</button>
-      </footer>
+      <form onSubmit={handleSubmit}>
+        <header className={styles.header}>
+          <p>새로운 룸의 이름 설정</p>
+        </header>
+        <div className={styles.content}>
+          <input value={text.text} onChange={TextHandler} type="text" />
+          <span>{text.length}/15</span>
+        </div>
+        <hr />
+        <footer className={styles.actions}>
+          <button type="button" onClick={props.onConfirm}>
+            취소
+          </button>
+          <div className={styles.updown} />
+          <button type="submit">완료</button>
+        </footer>
+      </form>
     </Card>
   );
 };
