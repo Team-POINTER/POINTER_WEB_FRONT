98d9090256641a6b3a42e1fadd8e4e292b0a8d80
pbit <alstjqrhkr2018@gmail.com>
2023-08-11 01:48:19 +0900

    [ADD] 힌트 작성하지 않고 버튼 클릭 시 -> 힌트 입력란에 포커싱

--- src/components/Hint/Hint.jsx
+++ src/components/Hint/Hint.jsx
@@ -13,7 +13,7 @@ export const Hint = (props) => {
 
   return (
     <div className={styles.container}>
-        <input className={styles.text} type="text" onChange={handleInputChange} value={props.hint} maxLength={20} />
+        <input id="hintInputField" className={styles.text} type="text" onChange={handleInputChange} value={props.hint} maxLength={20} />
         <span className={styles.num}>{props.hint.length} / 20</span>
     </div>
   );
