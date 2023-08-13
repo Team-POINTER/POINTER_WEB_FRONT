2ab187e56e326e1e5b6032cf81a286f576222f5c
pbit <alstjqrhkr2018@gmail.com>
2023-08-13 09:27:54 +0900

    [ADD] 자동완성 기능 해제

--- src/components/Hint/Hint.jsx
+++ src/components/Hint/Hint.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import styles from './Hint.module.css';
+import React, { useState } from "react";
+import styles from "./Hint.module.css";
 
 export const Hint = (props) => {
-
   const handleInputChange = (e) => {
     const inputValue = e.target.value;
 
@@ -13,8 +12,16 @@ export const Hint = (props) => {
 
   return (
     <div className={styles.container}>
-        <input id="hintInputField" className={styles.text} type="text" onChange={handleInputChange} value={props.hint} maxLength={20} />
-        <span className={styles.num}>{props.hint.length} / 20</span>
+      <input
+        id="hintInputField"
+        className={styles.text}
+        type="text"
+        onChange={handleInputChange}
+        value={props.hint}
+        maxLength={20}
+        autoComplete="off"
+      />
+      <span className={styles.num}>{props.hint.length} / 20</span>
     </div>
   );
 };
