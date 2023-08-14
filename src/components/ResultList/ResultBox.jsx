9e3420affe227a1d46146fe0b4f9f0d54e21fa65
pbit <alstjqrhkr2018@gmail.com>
2023-08-15 07:30:31 +0900

    [FEAT] 나의 결과 보기 박스 구현

--- src/components/ResultList/ResultBox.jsx
+++ src/components/ResultList/ResultBox.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./ResultBox.module.css";
 
 const ResultBox = ({ roomData }) => {
-  return <></>;
+  const sectionClickHandler = () => {
+    
+  }
+  return (
+    <section onClick={sectionClickHandler} className={styles.container}>
+      <div className={styles.question}>
+        {roomData.question}
+      </div>
+      <div className={styles.bottom}>
+        <span className={styles.vote}>{roomData.votedMemberCnt} / {roomData.allVoteCnt}</span>
+        <span className={styles.date}>23.03.02</span>
+      </div>
+    </section>
+  );
 };
 
 export default ResultBox;
