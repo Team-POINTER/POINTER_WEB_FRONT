707c39ebbbdcffa095cf3adad2c77d01380aa9f8
pbit <alstjqrhkr2018@gmail.com>
2023-08-15 07:34:33 +0900

    [FIX] 컴포넌트 Wrap 으로 처리

--- src/pages/MyResultList.jsx
+++ src/pages/MyResultList.jsx
@@ -1,27 +1,45 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Header } from "../components/Header/Header";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import { getCookie } from "../function/cookie";
 import { myResultList } from "../api/room";
+import { useDispatch } from "react-redux";
+import { RoomService } from "../service/RoomService";
+import RoomWrap from "../UI/RoomWrap/RoomWrap";
+import { RoomBox } from "../components/RoomList/RoomBox";
+import styled from "styled-components";
+import RoomContainer from "../UI/RoomContainer/RoomContainer";
+import ResultBox from "../components/ResultList/ResultBox";
+import Wrap from "../UI/Wrap/Wrap";
 
 const MyResultList = (props) => {
   const { state } = useLocation();
   const { roomData } = state;
+  const [roomList, setRoomList] = useState([]);
+
   useEffect(() => {
     const fetch = async () => {
       const response = await myResultList(roomData.roomId);
-      console.log(1);
+      setRoomList(response);
       console.log(response);
-      
-    }
+    };
     fetch();
-  });
+  }, []);
 
   return (
-    <Fragment>
+    <Wrap>
       <Header />
-    </Fragment>
+      {roomList && (
+        <RoomWrap>
+          <RoomContainer row={roomList.length}>
+            {roomList.map((room, index) => (
+              <ResultBox roomData={room} key={index}></ResultBox>
+            ))}
+          </RoomContainer>
+        </RoomWrap>
+      )}
+    </Wrap>
   );
 };
 
