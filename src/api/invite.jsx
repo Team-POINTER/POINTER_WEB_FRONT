bb95243f756aeebf86b83eef4f0cc69d91cf9133
pbit <alstjqrhkr2018@gmail.com>
2023-08-15 16:02:33 +0900

    [FEAT] 초대 가능 여부 판단 구현

--- src/api/invite.jsx
+++ src/api/invite.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { getCookie } from "../function/cookie";
 
-export const LinkInvite = ({invitationCode}) => {
-  return axios.get(
-    `${process.env.REACT_APP_BASE_URL}/room/invitation/${invitationCode}`,
-    {
-      headers: {
-        Authorization: "Bearer " + getCookie("refreshToken"),
-      },
-    }
-  );
+export const LinkInvite = (invitationCode) => {
+  return axios
+    .get(
+      `${process.env.REACT_APP_BASE_URL}/room/invitation/${invitationCode}`,
+      {
+        headers: {
+          Authorization: "Bearer " + getCookie("refreshToken"),
+        },
+      }
+    ).then(res => res.data);
 };
