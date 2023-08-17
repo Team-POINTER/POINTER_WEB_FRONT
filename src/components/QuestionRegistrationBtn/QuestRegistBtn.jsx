1b6be89b5de298a3e7186d85f50963ed30e722be
pbit <alstjqrhkr2018@gmail.com>
2023-08-17 10:33:57 +0900

    [FIX] 질문 등록하기 수직 수평 가운데로 이동

--- src/components/QuestionRegistrationBtn/QuestRegistBtn.jsx
+++ src/components/QuestionRegistrationBtn/QuestRegistBtn.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const QuestRegistBtn = (props) => {
   return (
     <button className={styles.btn} onClick={props.onClickHandler}>
-      질문 등록하기
+      <span>질문 등록하기</span>
     </button>
   );
 };
